d7db7d5e5c50e076ec402c52e81f0cc356a760dd
Jayashree <s.jayashree94@gmail.com>
2025-07-31 05:57:22 +0530

    tabform

--- Tabform.js
+++ Tabform.js
@@ -1,11 +1,27 @@
-import React from 'react'
-
-const Tabform = () => {
+const Profile = ({ data, setData }) => {
+  const { name, age, email } = data;
+  const handleDataChange = (e, item) => {
+    setData((prevState) => ({
+      ...prevState,
+      [item]: e.target.value,
+    }));
+  };
   return (
     <div>
-      
+      <div>
+        <label>Name : </label>
+        <input type="text" value={name} onChange={handleDataChange} />
+      </div>
+      <div>
+        <label>Age : </label>
+        <input type="text" value={age} onChange={handleDataChange} />
+      </div>
+      <div>
+        <label>Email : </label>
+        <input type="text" value={email} onChange={handleDataChange} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Tabform
+export default Profile;
