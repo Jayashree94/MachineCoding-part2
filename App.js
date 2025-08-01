ab9ed77db78d61a64d7a9a813bf2f6b4d3c8c36e
Jayashree <s.jayashree94@gmail.com>
2025-08-01 07:46:54 +0530

    otp

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./styles.css";
 const OTP_DIGITS = 5;
 
 export default function App() {
   const [inputArr, setInputArr] = useState(new Array(OTP_DIGITS).fill(""));
+  refArr = useRef([]);
+  useEffect(() => {
+    refArr.current[0]?.focus();
+  }, []);
   const handleOnChange = (e, index) => {
     if (isNaN(e.target.value)) return;
     const newArr = [...inputArr];
     newArr[index] = e.target.value.slice(-1);
     setInputArr(newArr);
+    refArr.current[index + 1]?.focus();
   };
   return (
     <div className="App">
@@ -19,6 +24,9 @@ export default function App() {
           key={index}
           type="text"
           value={inputArr[index]}
+          ref={(input) => {
+            refArr.current[index] = input;
+          }}
           onChange={(e) => handleOnChange(e, index)}
         ></input>
       ))}
